e2c23e7c40100892ca88b782c29127eb22b2b911
solgagou <solgagou@gmail.com>
2024-11-12 19:05:13 +0100

    feat: add other details on media queries on main page: max width 800 and 430

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,24 +7,24 @@ function Footer() {
   return (
     <div className="footer" id="footer">
       <div className='footer__info'>
-        <p className='footer__copyright'>2024 @ Loop Teatral | Sol Gago</p>
         <ul className="footer__icons">
-        <li>
-          <a href="https://www.linkedin.com/in/solgagou/" target="_blank" rel="noopener noreferrer">
-          <img src={linkedinIcon} className="linkedin_icon" alt="LinkedIn icon" />
+          <li>
+            <a href="https://www.linkedin.com/in/solgagou/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinIcon} className="linkedin_icon" alt="LinkedIn icon" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/solgagou" target="_blank" rel="noopener noreferrer">
+            <img src={gitIcon} className="github_icon" alt="GitHub icon" />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:solgagou@gmail.com" target="_blank" rel="noopener noreferrer">
+            <img src={emailIcon} className="email_icon" alt="Email icon" />
           </a>
-        </li>
-        <li>
-          <a href="https://github.com/solgagou" target="_blank" rel="noopener noreferrer">
-          <img src={gitIcon} className="github_icon" alt="GitHub icon" />
-          </a>
-        </li>
-        <li>
-          <a href="mailto:solgagou@gmail.com" target="_blank" rel="noopener noreferrer">
-          <img src={emailIcon} className="email_icon" alt="Email icon" />
-        </a>
-        </li>
-        </ul>  
+          </li>
+          </ul>  
+        <p className='footer__copyright'>2024 @ Loop Teatral | Sol Gago</p>
       </div>
     </div>
   )
